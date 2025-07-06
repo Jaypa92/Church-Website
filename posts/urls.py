6cb8184f34b111327df3a3d7734f944d074f9426
[Jaypa92] <justinparris.a@gmail.com>
2025-07-06 19:43:19 -0400

    started the admin page allowing some functions

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
+    path('admin-panel/new/', views.new_post, name='new_post'),
+    path('admin-panel/delete/<int:post_id>', views.delete_post, name='delete_post'),
     path('', views.post_list, name='post_list')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+#@login_required
+def admin_panel(request):
+    posts = Post.objects.all().order_by('created_at')
+    return render(request, 'posts/admin_panel.html', {'posts': posts})
+
+#@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_panel')
+    else:
+        form = PostForm()
+    return render(request, 'posts/new_post.html', {'form':form})
+
+#@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('admin_panel')
+
 def post_list(request):
     posts = Post.objects.all().order_by('-id')
     return render(request,'posts/post_list.html',{'posts':posts})
+
